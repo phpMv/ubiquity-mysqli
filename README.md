aac1e9d66dfdf361b8df021cc81f9d8402e38e12
jcheron <myaddressmail@gmail.com>
2019-08-15 02:38:56 +0200

    Mysqli initialization

---
+++ README.md
@@ -0,0 +1,8 @@
+# ubiquity-mysqli
+mysqli wrapper for Ubiquity php framework
+
+[![Latest Stable Version](https://poser.pugx.org/phpmv/ubiquity-mysqli/v/stable)](https://packagist.org/packages/phpmv/ubiquity-mysqli) [![Total Downloads](https://poser.pugx.org/phpmv/ubiquity-mysqli/downloads)](https://packagist.org/packages/phpmv/ubiquity-mysqli) [![Latest Unstable Version](https://poser.pugx.org/phpmv/ubiquity-mysqli/v/unstable)](https://packagist.org/packages/phpmv/ubiquity-mysqli) [![License](https://poser.pugx.org/phpmv/ubiquity-mysqli/license)](https://packagist.org/packages/phpmv/ubiquity-mysqli)
+
+## Composer dependencies
+- [phpmv/ubiquity](https://github.com/phpMv/ubiquity)
+- [mysqli]()
---
+++ src/Ubiquity/db/providers/mysqli/MysqliWrapper.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Ubiquity\db\providers\mysqli;
+
+use Ubiquity\db\providers\AbstractDbWrapper;
+
+/**
+ * Ubiquity\db\providers\mysqli$MysqliWrapper
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.0
+ * @property \mysqli $dbInstance
+ *
+ */
+class MysqliWrapper extends AbstractDbWrapper {
+	protected $transactionLevel = 0;
+	
+	protected function _execute(\mysqli_stmt $statement, array $values = null){
+		if($values!==null){
+			$statement->bind_param(str_repeat('s', sizeof($values)), ...$values);
+		}
+		return $statement->execute();
+	}
+	
+	public function __construct($dbType = 'mysql') {
+		$this->quote = '`';
+	}
+	
+	public function fetchAllColumn($statement, array $values = null, string $column = null) {
+		$result = false;
+		if ($this->_execute($statement,$values)) {
+			$res = $statement->get_result();
+			$result = [];
+			while ($row = $res->fetch_row()){
+				$result[] = $row[0];
+			}
+		}
+		$statement->close();
+		return $result;
+	}
+	
+	public function lastInsertId() {
+		return $this->dbInstance->insert_id;
+	}
+	
+	public function fetchAll($statement, array $values = null, $mode = null) {
+		$result = false;
+		if ($this->_execute ( $values )) {
+			$res = $statement->get_result();
+			$result = $res->fetch_all($mode);
+		}
+		$statement->close();
+		return $result;
+	}
+	
+	public function fetchOne($statement, array $values = null, $mode = null) {
+		$result = false;
+		if ($this->_execute ( $values )) {
+			$res = $statement->get_result();
+			$result = $res->fetch_array( $mode );
+		}
+		$statement->close();
+		return $result;
+	}
+	
+	public static function getAvailableDrivers() {
+		return 'mysql';
+	}
+	
+	public function prepareStatement(string $sql) {
+		return $this->dbInstance->prepare( $sql );
+	}
+	
+	public function fetchColumn($statement, array $values = null, int $columnNumber = null) {
+		if ($this->_execute ( $values )) {
+			$res = $statement->get_result();
+			return $res->fetch_row()[$columnNumber??0];
+		}
+		return false;
+	}
+	
+	public function getStatement($sql) {
+		return $this->dbInstance->prepare ( $sql );
+	}
+	
+	public function execute($sql) {
+		return $this->dbInstance->real_query( $sql );
+	}
+	
+	public function connect(string $dbType, $dbName, $serverName, string $port, string $user, string $password, array $options) {
+		$this->dbInstance = new \mysqli( $serverName,$user,$password,$dbName, $port);
+		$this->dbInstance->set_charset("utf8");
+		foreach ($options as $key=>$value){
+			$this->dbInstance->set_opt($key, $value);
+		}
+	}
+	
+	public function getDSN(string $serverName, string $port, string $dbName, string $dbType = 'mysql') {
+		return 'mysqli:dbname=' . $dbName . ';host=' . $serverName . ';charset=UTF8;port=' . $port;
+	}
+	
+	public function bindValueFromStatement($statement, $parameter, $value) {
+		return $statement->bind_param( 's' ,$value);
+	}
+	
+	public function query(string $sql) {
+		return $this->dbInstance->query( $sql );
+	}
+	
+	public function queryAll(string $sql, int $fetchStyle = null) {
+		return $this->dbInstance->query ( $sql )->fetch_all( $fetchStyle );
+	}
+	
+	public function queryColumn(string $sql, int $columnNumber = null) {
+		return $this->dbInstance->query ( $sql )->fetch_row()[$columnNumber??0];
+	}
+	
+	public function executeStatement($statement, array $values = null) {
+		return $this->_execute ( $values );
+	}
+	
+	public function getTablesName() {
+		$query = $this->dbInstance->query ( 'SHOW TABLES' );
+		$result=[];
+		while ($row = $query->fetch_row()){
+			$result[] = $row[0];
+		}
+		return $result;
+	}
+	
+	public function statementRowCount(\mysqli_stmt $statement) {
+		return $statement->num_rows;
+	}
+	
+	public function inTransaction() {
+		return null;
+	}
+	
+	public function commit() {
+		return $this->dbInstance->commit();
+	}
+	
+	public function rollBack() {
+		return $this->dbInstance->rollBack ();
+	}
+	
+	public function beginTransaction() {
+		return $this->dbInstance->begin_transaction();
+	}
+	
+	public function savePoint($level) {
+		$this->dbInstance->savepoint($level);
+	}
+	
+	public function releasePoint($level) {
+		$this->dbInstance->release_savepoint($level);
+	}
+	
+	public function rollbackPoint($level) {
+		$this->dbInstance->rollback($level);
+	}
+	
+	public function nestable() {
+		return true;
+	}
+	
+	public function ping() {
+		return $this->dbInstance->ping();
+	}
+	
+	public function getPrimaryKeys($tableName) {
+		$fieldkeys = array ();
+		$recordset = $this->dbInstance->query ( "SHOW KEYS FROM `{$tableName}` WHERE Key_name = 'PRIMARY'" );
+		$keys = $recordset->fetch_all( \MYSQLI_ASSOC );
+		foreach ( $keys as $key ) {
+			$fieldkeys [] = $key ['Column_name'];
+		}
+		return $fieldkeys;
+	}
+	
+	public function getForeignKeys($tableName, $pkName, $dbName = null) {
+		$recordset = $this->dbInstance->query ( "SELECT *
+												FROM
+												 information_schema.KEY_COLUMN_USAGE
+												WHERE
+												 REFERENCED_TABLE_NAME = '" . $tableName . "'
+												 AND REFERENCED_COLUMN_NAME = '" . $pkName . "'
+												 AND TABLE_SCHEMA = '" . $dbName . "';" );
+		return $recordset->fetchAll ( \PDO::FETCH_ASSOC );
+	}
+	
+	public function getFieldsInfos($tableName) {
+		$fieldsInfos = array ();
+		$recordset = $this->dbInstance->query ( "SHOW COLUMNS FROM `{$tableName}`" );
+		$fields = $recordset->fetch_all( \MYSQLI_ASSOC );
+		foreach ( $fields as $field ) {
+			$fieldsInfos [$field ['Field']] = [ "Type" => $field ['Type'],"Nullable" => $field ["Null"] ];
+		}
+		return $fieldsInfos;
+	}
+}
